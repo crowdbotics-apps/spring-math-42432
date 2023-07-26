5d5047dc92bc91a20f6e0f473c199ef20917155a
Crowdbotics <team@crowdbotics.com>
2023-07-26 02:08:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-math-42432",
+  url: "https://spring-math-42432.botics.co",
+  api: "https://spring-math-42432.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
